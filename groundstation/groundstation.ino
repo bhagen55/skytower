5b71861e4c80a544ebd2d939c9345ee0b6406506
Blair Hagen <bhagen55@gmail.com>
2019-03-07 17:46:27 -0500

    Changed back to transparent

--- groundstation/groundstation.ino
+++ groundstation/groundstation.ino
@@ -6,8 +6,8 @@ const byte ppmPin = 20;
 const byte statusLed = 13;
 
 /*
- * --- PPM ---
- */
+   --- PPM ---
+*/
 // vars
 const int ppmChannels = 8;
 uint16_t channelVals[ppmChannels];
@@ -16,28 +16,20 @@ uint16_t channelVals[ppmChannels];
 // ppm reader
 PulsePositionInput ppmIn;
 
-/*
- * --- XBee ---
- */
-// vars
-XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x415463DE);
-ZBTxRequest zbTx = ZBTxRequest(addr64, (uint8_t *)channelVals, sizeof(channelVals));
-ZBTxStatusResponse txStatus = ZBTxStatusResponse();
-
-// XBee obj
-XBee xbee = XBee();
+// Serial sender
+char channelBuff[(ppmChannels * 2) + 3]; 
 
 
 /*
-Get the values of ppm channels and store it in an array
-TODO: use .available() to find available channels and dynamically allocate array.
+  Get the values of ppm channels and store it in an array
+  TODO: use .available() to find available channels and dynamically allocate array.
       Use first array spot to hold number of channels being sent.
 */
 void getChannelVals(int channels, uint16_t channelArr[]) {
   if (ppmIn.available() == ppmChannels) {
     for (int chan = 1; chan <= channels; chan++) {
-    channelArr[chan - 1] = ppmIn.read(chan);
-    //channelArr[chan] = 1500;
+      channelArr[chan - 1] = ppmIn.read(chan);
+      //channelArr[chan] = 1500;
     }
   } else {
     Serial.println("No PPM or wrong number of PPM channels");
@@ -46,8 +38,8 @@ void getChannelVals(int channels, uint16_t channelArr[]) {
 
 
 /*
- * Flash error led
- */
+   Flash error led
+*/
 void flashLed(int pin, int times, int wait) {
   for (int i = 0; i < times; i++) {
     digitalWrite(pin, HIGH);
@@ -60,8 +52,30 @@ void flashLed(int pin, int times, int wait) {
   }
 }
 
+void loadChannelBuff(int channels, uint16_t channelArr[], char buff[]) {
+  int pIndex = 0;
+
+  buff[pIndex] = '[';
+
+  pIndex++;
+  
+
+  for (int chan = 0; chan <= channels; chan++) {
+    buff[pIndex] = channelArr[chan] >> 8 & 0xff;
+    pIndex++;
+    buff[pIndex] = channelArr[chan] & 0xff;
+    pIndex++;
+  }
+
+  buff[pIndex] = ']';
+
+  buff[pIndex+1] = '\0';
+
+}
+
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(statusLed, OUTPUT);
+  
   // PPM
   ppmIn.begin(ppmPin);
 
@@ -70,44 +84,19 @@ void setup() {
 
   // XBee Serial
   Serial1.begin(9600);
-  xbee.setSerial(Serial1);
+
+  flashLed(statusLed, 3, 50);
+  Serial.println("GS Starting...");
 }
 
 void loop() {
+  //Serial.println("Sending");
   getChannelVals(ppmChannels, channelVals);
-  Serial.print("Payload size: ");
-  Serial.println(sizeof(channelVals));
-  
+  loadChannelBuff(ppmChannels, channelVals, channelBuff);
 
-  flashLed(statusLed, 1, 50);
-  xbee.send(zbTx);
+  Serial1.write(channelBuff);
+  Serial.write(channelBuff);
+  Serial.print("hello");
 
-  if (xbee.readPacket(1000)) {
-    Serial.println("Response Received");
-    // Response received
-    // should be a znet tx status              
-    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
-      xbee.getResponse().getZBTxStatusResponse(txStatus);
-      // get the delivery status, the fifth byte
-      if (txStatus.getDeliveryStatus() == SUCCESS) {
-        // success, packet delivered
-        Serial.println("Info: Packet delivered");
-        flashLed(statusLed, 5, 50);
-      } else {
-        // the remote XBee did not receive our packet
-        Serial.println("Error: Remote XBee did not receive packet");
-        flashLed(statusLed, 3, 500);
-      }
-    }
-  } else if (xbee.getResponse().isError()) {
-    // Error received
-    Serial.print("Error: Error reading packet.  Error code: ");  
-    Serial.println(xbee.getResponse().getErrorCode());
-  } else {
-    // local XBee did not provide a timely TX Status Response -- should not happen
-    Serial.println("Error: Timeout with local XBee");
-    flashLed(statusLed, 2, 250);
-  }
-
-  delay(50);
+  delay(200);
 }
--- remote/remote.ino
+++ remote/remote.ino
@@ -1,114 +1,67 @@
 #include <XBee.h>
 
-/*
- * --- XBee ---
- */
-XBee xbee = XBee();
-XBeeResponse response = XBeeResponse();
-ZBRxResponse rx = ZBRxResponse();
-ModemStatusResponse msr = ModemStatusResponse();
-
 int statusLed = 13;
 
 const int channels = 8;
 uint16_t channelVals[channels];
 
+char buff[100];
+int buffInd = 0;
+char recv;
+bool received = false;
 
 /*
- * Flash error led
- */
+   --- XBee ---
+*/
+
+
+/*
+   Flash error led
+*/
 void flashLed(int pin, int times, int wait) {
-    for (int i = 0; i < times; i++) {
-      digitalWrite(pin, HIGH);
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+
+    if (i + 1 < times) {
       delay(wait);
-      digitalWrite(pin, LOW);
-      
-      if (i + 1 < times) {
-        delay(wait);
-      }
     }
+  }
 }
+
+
 
 void setup() {
   pinMode(statusLed, OUTPUT);
-  
+
   // start serial
   Serial.begin(9600);
 
   // start XBee serial
   Serial1.begin(9600);
-  xbee.begin(Serial1);
-  
+
   flashLed(statusLed, 3, 50);
-  Serial.println("Starting...");
+  Serial.println("Remote Starting...");
 }
 
-// continuously reads packets, looking for ZB Receive or Modem Status
 void loop() {
-    flashLed(statusLed, 1, 50);
-    xbee.readPacket();
-    
-    if (xbee.getResponse().isAvailable()) {
-      Serial.println("Got packet");
-      // got something
-      
-      if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
-        Serial.println("Got a receipt packet");
-        // got a zb rx packet
-        
-        // now fill our zb rx class
-        xbee.getResponse().getZBRxResponse(rx);
-            
-        if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED) {
-            Serial.println("Sender received our ack");
-            // the sender got an ACK
-            flashLed(statusLed, 10, 10);
-        } else {
-            Serial.println("Sender did not receive our ack");
-            Serial.print("getOption: ");
-            Serial.println(rx.getOption(), HEX);
-            // we got it (obviously) but sender didn't get an ACK
-            flashLed(statusLed, 2, 20);
-        }
-
-        // Process Channel Data
-        Serial.print("Data length: ");
-        Serial.println(rx.getDataLength());
-
-        if (rx.getDataLength() == sizeof(channelVals)) {
-          for (int i = 0; i < channels; i++) {
-            channelVals[i] = rx.getData(i*2) | rx.getData(i*2+1) << 8;
-            Serial.println(channelVals[i]);
-          }
-        } else { // received data length doesn't match up with channel
-          Serial.println("Data received not of correct size");
-          Serial.print("Was of size ");
-          Serial.println(rx.getDataLength());
-        }
-        
-      } else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) {
-        xbee.getResponse().getModemStatusResponse(msr);
-        // the local XBee sends this response on certain events, like association/dissociation
-        
-        if (msr.getStatus() == ASSOCIATED) {
-          Serial.println("Associated with other XBee");
-          // yay this is great.  flash led
-          flashLed(statusLed, 10, 10);
-        } else if (msr.getStatus() == DISASSOCIATED) {
-          Serial.println("Disassociated with other XBee");
-          // this is awful.. flash led to show our discontent
-          flashLed(statusLed, 10, 10);
-        } else {
-          // another status
-          flashLed(statusLed, 5, 10);
-        }
-      } else {
-        // not something we were expecting
-        Serial.println("Something unexpected happened");
-        flashLed(statusLed, 1, 25);    
+  while (Serial1.available() > 0) {
+    char recv = Serial1.read();
+    if (recv == '[') {
+      while (recv != ']') {
+        buff[buffInd] = recv;
+        recv = Serial1.read();
+        buffInd++;
       }
-    } else if (xbee.getResponse().isError()) {
-      Serial.print("Error reading packet.  Error code: ");  
-      Serial.println(xbee.getResponse().getErrorCode());
+      buff[buffInd] = recv;
+      buff[buffInd + 1] = '\0';
+      received = true;
+      Serial.println(buff);
+      buffInd = 0;
+      received = false;
     }
+  }
+  //Serial.println("No data available");
+  delay(50);
 }
